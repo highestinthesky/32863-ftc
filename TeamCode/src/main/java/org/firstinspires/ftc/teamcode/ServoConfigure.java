368bf05ad4eea06e0f51ddd1e2d69ba142bf8a94
highestinthesky <haolunzhang@hunterschools.org>
2026-02-20 19:43:51 -0500

    Fix ServoConfigure null crash when right turret servo is missing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoConfigure.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoConfigure.java
@@ -10,37 +10,37 @@ public class ServoConfigure extends OpMode {
     private Servo leftturretturn;
     public double rightturretturnvalue;
     public double leftturretturnvalue;
+
     @Override
     public void init() {
-        rightturretturn = hardwareMap.get(Servo.class, "rightturretturn");
-        leftturretturn = hardwareMap.get(Servo.class, "leftturretturn");
-        rightturretturn.setDirection(Servo.Direction.FORWARD);
+        try {
+            rightturretturn = hardwareMap.get(Servo.class, "rightturretturn");
+            rightturretturn.setDirection(Servo.Direction.FORWARD);
+        } catch (IllegalArgumentException ignored) {
+            telemetry.addLine("rightturretturn not found in configuration");
+        }
+
+        leftturretturn = hardwareMap.get(Servo.class, "tservo");
         leftturretturn.setDirection(Servo.Direction.FORWARD);
 
         telemetry.addLine("init done");
     }
 
     @Override
-    public void start(){
-        rightturretturn.setPosition(0);
+    public void start() {
+        if (rightturretturn != null) {
+            rightturretturn.setPosition(0);
+        }
         leftturretturn.setPosition(0);
     }
+
     @Override
     public void loop() {
-        telemetry.update();
         int action = 0;
-        if (gamepad1.dpadUpWasPressed()) action = 1;
-        else if (gamepad1.dpadDownWasPressed()) action = 2;
-        else if (gamepad1.yWasPressed()) action  = 3;
-        else if (gamepad1.aWasPressed()) action  = 4;
+        if (gamepad1.yWasPressed()) action = 3;
+        else if (gamepad1.aWasPressed()) action = 4;
 
         switch (action) {
-            case 1:
-                if (rightturretturnvalue < 1) rightturretturnvalue += 0.1;
-                break;
-            case 2:
-                if (rightturretturnvalue > 0) rightturretturnvalue -= 0.1;
-                break;
             case 3:
                 if (leftturretturnvalue < 1) leftturretturnvalue += 0.1;
                 break;
@@ -48,7 +48,10 @@ public class ServoConfigure extends OpMode {
                 if (leftturretturnvalue > 0) leftturretturnvalue -= 0.1;
         }
 
-        rightturretturn.setPosition(rightturretturnvalue);
+        if (rightturretturn != null) {
+            rightturretturn.setPosition(rightturretturnvalue);
+        }
         leftturretturn.setPosition(leftturretturnvalue);
+        telemetry.update();
     }
 }
